d79775e231830c11d292d5b8bbffe76ccc4a0c62
David Ren <dren391@gmail.com>
2020-04-23 17:30:08 -0400

    adjusted line spacing

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  console.log('DOM fully loaded and parsed');
+    console.log('DOM fully loaded and parsed');
 
-  // ------------------- Selector Variables --------------------
-  
-  const navbar = document.querySelector('[data-navbar="navbar"]');
-  const productTab = document.querySelector('[data-products="products-tab"]');
-  const industryTab = document.querySelector('[data-industries="industries-tab"]');
-  const dropdown = document.querySelector('[data-dropdown="dropdown"]');
-  const productDropdown = document.querySelector('[data-dropdown-products="dropdown-products"]')
-  const industryDropdown = document.querySelector('[data-dropdown-industries="dropdown-industries"]')
+    // ------------------- Selector Variables --------------------
+
+    const navbar = document.querySelector('[data-navbar="navbar"]');
+    const productTab = document.querySelector('[data-products="products-tab"]');
+    const industryTab = document.querySelector('[data-industries="industries-tab"]');
+    const dropdown = document.querySelector('[data-dropdown="dropdown"]');
+    const productDropdown = document.querySelector('[data-dropdown-products="dropdown-products"]')
+    const industryDropdown = document.querySelector('[data-dropdown-industries="dropdown-industries"]')
 
 
-  // --------------------- Functions ----------------------------
+    // --------------------- Functions ----------------------------
 
-  function showProducts() {
-      industryDropdown.classList.remove('industries-container');
-      industryDropdown.classList.add('hide-industries')
-      productDropdown.classList.remove('hide-products');
-      productDropdown.classList.add('products-container');
-      dropdown.classList.remove('hide-dropdown');
-      dropdown.classList.add('dropdown-menu');
-  }
+    function showProducts() {
+        industryDropdown.classList.remove('industries-container');
+        industryDropdown.classList.add('hide-industries')
+        productDropdown.classList.remove('hide-products');
+        productDropdown.classList.add('products-container');
+        dropdown.classList.remove('hide-dropdown');
+        dropdown.classList.add('dropdown-menu');
+    }
 
-  function showIndustries() {
+    function showIndustries() {
     productDropdown.classList.remove('products-container');
     productDropdown.classList.add('hide-products');
     industryDropdown.classList.remove('hide-industries');
     industryDropdown.classList.add('industries-container')
     dropdown.classList.remove('hide-dropdown');
     dropdown.classList.add('dropdown-menu');
-  }
+    }
 
-  function hideDropdown() {
+    function hideDropdown() {
     productDropdown.classList.remove('products-container');
     productDropdown.classList.add('hide-products');
     industryDropdown.classList.remove('industries-container');
     industryDropdown.classList.add('hide-industries');
     dropdown.classList.remove('dropdown-menu');
     dropdown.classList.add('hide-dropdown');
-  }
-  // -------------------- Event Listener ------------------------
+    }
+    // -------------------- Event Listener ------------------------
 
-  navbar.addEventListener('mouseover', event => {
+    navbar.addEventListener('mouseover', event => {
     if(event.target.dataset.products === "products-tab") {
-      showProducts();
+        showProducts();
     } else if(event.target.dataset.industries === "industries-tab") {
-      showIndustries();
+        showIndustries();
     } else {
         hideDropdown();
     }
-  })
+    })
 
 });  
